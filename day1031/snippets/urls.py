ef99953d6a80ca1e868ccb7ceee104a2a46e3d7c
dropsong <yanchenglyc@outlook.com>
2024-11-04 12:57:21 +0800

    DRF example

---
+++ day1031/snippets/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail),
+]
+
+# 为了让 url 支持 json
+urlpatterns = format_suffix_patterns(urlpatterns)
---
+++ day1031/snippets/views_v2.py
@@ -0,0 +1,58 @@
+# Django 视图
+# views.py 和 views_v2.py 只能有一个能成为 views.py
+# 谁成为 views.py 谁就是真正起作用的那个
+
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+@csrf_exempt
+def snippet_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST': # 新建
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':   # 详情页的查询
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT': # 修改
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':  # 删除
+        snippet.delete()
+        return HttpResponse(status=204)
